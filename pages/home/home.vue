5a02a23ee12fa85e2909ebf02cebff66c51a6777
Linst-inf <Linst8350@163.com>
2023-02-25 15:45:48 +0800

    完成search开发

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,9 @@
 <template>
 	<view>
+		<!-- 使用自定义的搜索组件 -->
+		<view class="search-box">
+			<my-search @click="gotoSearch"></my-search>
+		</view>
 		<!-- 轮播图区域 uswiper快速生成 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
 			<!-- 循环渲染轮播图的 item 项 -->
@@ -31,7 +35,8 @@
 					</navigator>
 					<!-- 右侧 4 个小图片的盒子 -->
 					<view class="right-img-box">
-						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2" v-if="i2 !== 0" :url="item2.url">
+						<navigator class="right-img-item" v-for="(item2, i2) in item.product_list" :key="i2"
+							v-if="i2 !== 0" :url="item2.url">
 							<image :src="item2.image_src" mode="widthFix" :style="{width: item2.image_width + 'rpx'}">
 							</image>
 						</navigator>
@@ -104,7 +109,12 @@
 				})
 				this.floorList = res.message
 			},
-		},
+			gotoSearch() {
+				uni.navigateTo({
+					url: '/subpkg/search/search'
+				})
+			}
+		}
 	}
 </script>
 
@@ -151,5 +161,14 @@
 	.floor-img-box {
 		display: flex;
 		padding-left: 10rpx;
+	}
+
+	.search-box {
+		// 设置定位效果为“吸顶”
+		position: sticky;
+		// 吸顶的“位置”
+		top: 0;
+		// 提高层级，防止被轮播图覆盖
+		z-index: 999;
 	}
 </style>
