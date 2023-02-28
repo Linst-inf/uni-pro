4cb50fa02ca679df3bd654cb1e952e108b269282
Linst-inf <Linst8350@163.com>
2023-02-28 15:29:34 +0800

    完成支付功能开发

--- main.js
+++ main.js
@@ -7,7 +7,9 @@ import Vue from 'vue'
 import store from './store/store.js'
 
 // 按需导入 $http 对象
-import { $http } from '@escook/request-miniprogram'
+import {
+	$http
+} from '@escook/request-miniprogram'
 // 在 uni-app 项目中，可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
 uni.$http = $http
 
@@ -15,41 +17,51 @@ uni.$http = $http
 $http.baseUrl = 'https://api-hmugo-web.itheima.net'
 
 // 请求开始之前做一些事情 请求拦截器
-$http.beforeRequest = function (options) {
-  uni.showLoading({
-	  title:'数据加载中...'
-  })
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...'
+	})
+	// 判断请求的是否为有权限的 API 接口
+	if (options.url.indexOf('/my/') !== -1) {
+		// 为请求头添加身份认证字段
+		options.header = {
+			// 字段的值可以直接从 vuex 中进行获取
+			Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjIzLCJpYXQiOjE1NjQ3MzAwNzksImV4cCI6MTAwMTU2NDczMDA3OH0.YPt-XeLnjV-_1ITaXGY2FhxmCe4NvXuRnRB8OMCfnPo",
+		}
+	}
 }
 
 // 请求完成之后做一些事情 相应拦截器
-$http.afterRequest = function () {
-  uni.hideLoading()
+$http.afterRequest = function() {
+	uni.hideLoading()
 }
 // 封装的展示消息提示的方法
-uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
-  uni.showToast({
-    title,
-    duration,
-    icon: 'none',
-  })
+uni.$showMsg = function(title = '数据加载失败！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none',
+	})
 }
 
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App,
-	  // 2. 将 store 挂载到 Vue 实例上
-	  store,
+	...App,
+	// 2. 将 store 挂载到 Vue 实例上
+	store,
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
